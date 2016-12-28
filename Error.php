d5d45a50bff4d9b552955cc41333104c39bf042f
sunnnnn <263021000@qq.com>
2016-12-28 10:34:18 +0800

    add all

---
+++ Error.php
@@ -0,0 +1,37 @@
+<?php
+namespace sunnnnn\wechat;
+
+class Error{
+    /**
+     * showError
+     * 显示错误信息
+     */
+    public static function showError($msg){
+?>
+		<html>
+		<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width,initial-scale=1,user-scalable=0">
+		<title>Error</title>
+		<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/zui/1.5.0/css/zui.min.css">
+		</head>
+		<body>
+			<div class="container">
+				<div class="row" style="margin-top:50px;">
+					<div class="col-md-12">
+						<div class="alert alert-danger-inverse with-icon">
+						  	<i class="icon-remove-sign"></i>
+						  	<div class="content">
+						  		<h4>出错啦</h4>
+								<p><?= $msg; ?></p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</body>
+		</html>
+<?php
+	exit();
+    }
+}
---
+++ README.md
@@ -0,0 +1,11 @@
+微信借口开发，集成主流框架（thinkphp5）
+=================
+#配置
+config.php中放入配置信息：
+'workerman' => [
+		'app_id' => 'wxedb9dfa9e01d25b3',  //app id
+		'app_secret' => '1df9902250f31fd0d996e8d24fa9d634', //app secret
+		'mch_id' => '', //商户号
+		'mch_key' => '', //商户 key
+		'cert_path' => '', //证书路径
+	],
